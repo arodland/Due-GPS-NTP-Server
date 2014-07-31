cd31d845350ea7cffa9218919fc36a09473057ad
Andrew Rodland <andrew@cleverdomain.org>
2014-07-30 21:20:21 -0400

    Do two timers. Get an overflow int from TC0 Ch0, and capture on Ch0/1

--- clock.ino
+++ clock.ino
@@ -1,34 +1,58 @@
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(115200);
+  Serial1.begin(4800);
   pmc_enable_periph_clk(TC_INTERFACE_ID + 0);
   TC_Configure(TC0, 0,
     TC_CMR_TCCLKS_XC0 |
-//    TC_CMR_TCCLKS_TIMER_CLOCK2 |
     TC_CMR_WAVSEL_UP_RC |
     TC_CMR_LDRA_RISING |
     TC_CMR_CPCTRG
   );
-  TC0->TC_CHANNEL[0].TC_IER = TC_IER_COVFS | TC_IER_CPCS | TC_IER_LDRAS;
+  TC0->TC_CHANNEL[0].TC_IER = TC_IER_CPCS | TC_IER_LDRAS;
+  TC0->TC_CHANNEL[0].TC_IDR = ~(TC_IER_CPCS | TC_IER_LDRAS);
+  TC0->TC_CHANNEL[0].TC_CCR = TC_CCR_CLKEN;
+  TC0->TC_CHANNEL[0].TC_RC = 10000000;
   NVIC_EnableIRQ(TC0_IRQn);
-  PIO_Configure(PIOB, PIO_PERIPH_B, PIO_PB25B_TIOA0, PIO_DEFAULT);
-  TC_SetRC(TC0, 0, 10000000);
-  TC_Start(TC0, 0);
+
+  pmc_enable_periph_clk(TC_INTERFACE_ID + 1);
+  TC_Configure(TC0, 1,
+    TC_CMR_TCCLKS_XC0 |
+    TC_CMR_WAVSEL_UP_RC |
+    TC_CMR_LDRA_RISING |
+    TC_CMR_CPCTRG
+  );
+  TC0->TC_CHANNEL[1].TC_IER = TC_IER_LDRAS;
+  TC0->TC_CHANNEL[1].TC_IDR = ~(TC_IER_LDRAS);
+  TC0->TC_CHANNEL[1].TC_CCR = TC_CCR_CLKEN;
+  TC0->TC_CHANNEL[1].TC_RC = 10000000;
+
+  TC0->TC_BCR = TC_BCR_SYNC;
+  
+  NVIC_EnableIRQ(TC1_IRQn);
 }
 
 void TC0_Handler() {
   static int ct = 0;
   uint32_t status = TC0->TC_CHANNEL[0].TC_SR;
   if (status & TC_SR_CPCS) {
+    Serial.print("TICK: ");
     Serial.println(ct++);
   }
-  if (status & TC_SR_COVFS) {
-    Serial.println("OVERFLOW!");
-  }
   if (status & TC_SR_LDRAS) {
-    Serial.print("CAPT: ");
+    Serial.print("CAPT 0: ");
     uint32_t tm = TC0->TC_CHANNEL[0].TC_RA;
     TC0->TC_CHANNEL[0].TC_RB;
+    Serial.println(tm);
+  }
+}
+
+void TC1_Handler() {
+  uint32_t status = TC0->TC_CHANNEL[1].TC_SR;
+  if (status & TC_SR_LDRAS) {
+    Serial.print("CAPT 1: ");
+    uint32_t tm = TC0->TC_CHANNEL[1].TC_RA;
+    TC0->TC_CHANNEL[1].TC_RB;
     Serial.println(tm);
   }
 }
