f78078800dea0b255869ddd474e81dc5c80e0bac
Andrew Rodland <andrew@cleverdomain.org>
2014-09-24 22:36:52 -0400

    Delete some debug messages and convert the remainder to debug() instead of printf

--- ethernet.cpp
+++ ethernet.cpp
@@ -141,53 +141,33 @@ static uint16_t emac_icmp_checksum(uint16_t *p_buff, uint32_t ul_len)
 
 static void emac_display_ip_packet(p_ip_header_t p_ip_header, uint32_t ul_size)
 {
-	printf("======= IP %4d bytes, HEADER ==========\r\n", (int)ul_size);
-	printf(" IP Version        = v.%d", (p_ip_header->ip_hl_v & 0xF0) >> 4);
-	printf("\r\n Header Length     = %d", p_ip_header->ip_hl_v & 0x0F);
-	printf("\r\n Type of service   = 0x%x", p_ip_header->ip_tos);
-	printf("\r\n Total IP Length   = 0x%X",
-			(((p_ip_header->ip_len) >> 8) & 0xff) +
-			(((p_ip_header->ip_len) << 8) & 0xff00));
-	printf("\r\n ID                = 0x%X",
-			(((p_ip_header->ip_id) >> 8) & 0xff) +
-			(((p_ip_header->ip_id) << 8) & 0xff00));
-	printf("\r\n Header Checksum   = 0x%X",
-			(((p_ip_header->ip_sum) >> 8) & 0xff) +
-			(((p_ip_header->ip_sum) << 8) & 0xff00));
-	puts("\r Protocol          = ");
-
 	switch (p_ip_header->ip_p) {
 	case IP_PROT_ICMP:
-		puts("ICMP");
+                debug("ICMP");
 		break;
 
 	case IP_PROT_IP:
-		puts("IP");
+                debug("IP");
 		break;
 
 	case IP_PROT_TCP:
-		puts("TCP");
+                debug("TCP");
 		break;
 
 	case IP_PROT_UDP:
-		puts("UDP");
+                debug("UDP");
 		break;
 
 	default:
-		printf("%d (0x%X)", p_ip_header->ip_p, p_ip_header->ip_p);
+                debug("Proto "); debug(p_ip_header->ip_p);
 		break;
 	}
 
-	printf("\r\n IP Src Address    = %d:%d:%d:%d",
-			p_ip_header->ip_src[0],
-			p_ip_header->ip_src[1],
-			p_ip_header->ip_src[2], p_ip_header->ip_src[3]);
-
-	printf("\r\n IP Dest Address   = %d:%d:%d:%d",
-			p_ip_header->ip_dst[0],
-			p_ip_header->ip_dst[1],
-			p_ip_header->ip_dst[2], p_ip_header->ip_dst[3]);
-	puts("\r\n----------------------------------------\r");
+        debug(" packet from ");
+        debug(p_ip_header->ip_src[0]); debug(".");
+        debug(p_ip_header->ip_src[1]); debug(".");
+        debug(p_ip_header->ip_src[2]); debug(".");
+        debug(p_ip_header->ip_src[3]); debug("\r\n");
 }
 
 static void emac_process_arp_packet(uint8_t *p_uc_data, uint32_t ul_size)
@@ -199,14 +179,6 @@ static void emac_process_arp_packet(uint8_t *p_uc_data, uint32_t ul_size)
 	p_arp_header_t p_arp = (p_arp_header_t) (p_uc_data + ETH_HEADER_SIZE);
 
 	if (SWAP16(p_arp->ar_op) == ARP_REQUEST) {
-		printf("-- IP  %d.%d.%d.%d\r\n",
-				p_arp->ar_spa[0], p_arp->ar_spa[1],
-				p_arp->ar_spa[2], p_arp->ar_spa[3]);
-
-		printf("-- IP  %d.%d.%d.%d\r\n",
-				p_arp->ar_tpa[0], p_arp->ar_tpa[1],
-				p_arp->ar_tpa[2], p_arp->ar_tpa[3]);
-
 		// ARP reply operation
 		p_arp->ar_op = SWAP16(ARP_REPLY);
 
@@ -225,7 +197,7 @@ static void emac_process_arp_packet(uint8_t *p_uc_data, uint32_t ul_size)
 		}
 		ul_rc = emac_dev_write(&gs_emac_dev, p_uc_data, ul_size, NULL);
 		if (ul_rc != EMAC_OK) {
-			printf("E: ARP Send - 0x%x\r\n", ul_rc);
+                        debug("ARP send error: 0x"); debug_hex(ul_rc); debug("\r\n");
 		}
 	}
 }
@@ -244,12 +216,6 @@ static void emac_process_ip_packet(uint8_t *p_uc_data, uint32_t ul_size)
 	p_icmp_echo_header_t p_icmp_echo =
 			(p_icmp_echo_header_t) ((int8_t *) p_ip_header +
 			ETH_IP_HEADER_SIZE);
-	printf("-- IP  %d.%d.%d.%d\r\n", p_eth->et_dest[0], p_eth->et_dest[1],
-			p_eth->et_dest[2], p_eth->et_dest[3]);
-
-	printf("-- IP  %d.%d.%d.%d\r\n",
-			p_eth->et_src[0], p_eth->et_src[1], p_eth->et_src[2],
-			p_eth->et_src[3]);
 	switch (p_ip_header->ip_p) {
 	case IP_PROT_ICMP:
 		if (p_icmp_echo->type == ICMP_ECHO_REQUEST) {
@@ -283,7 +249,7 @@ static void emac_process_ip_packet(uint8_t *p_uc_data, uint32_t ul_size)
 			ul_rc = emac_dev_write(&gs_emac_dev, p_uc_data,
 					SWAP16(p_ip_header->ip_len) + 14, NULL);
 			if (ul_rc != EMAC_OK) {
-				printf("E: ICMP Send - 0x%x\r\n", ul_rc);
+                                debug("ICMP send error: 0x"); debug_hex(ul_rc); debug("\r\n");
 			}
 		}
 		break;
@@ -323,14 +289,13 @@ static void emac_process_eth_packet(uint8_t *p_uc_data, uint32_t ul_size)
 		break;
 
 	default:
-		printf("=== Default w_pkt_format= 0x%X===\r\n", us_pkt_format);
 		break;
 	}
 }
 
 void EMAC_Handler(void)
 {
-	Serial.print("EH\r\n");
+	debug("EH\r\n");
         ether_int = 1;
 	emac_handler(&gs_emac_dev);
 }
@@ -343,12 +308,19 @@ void ether_init() {
 	ethernet_pio_setup();
 
  	// Display MAC & IP settings
-	printf("-- MAC %x:%x:%x:%x:%x:%x\r\n",
-			gs_uc_mac_address[0], gs_uc_mac_address[1], gs_uc_mac_address[2],
-			gs_uc_mac_address[3], gs_uc_mac_address[4], gs_uc_mac_address[5]);
+        debug("MAC: ");
+        debug_hex(gs_uc_mac_address[0]); debug(":");
+        debug_hex(gs_uc_mac_address[1]); debug(":");
+        debug_hex(gs_uc_mac_address[2]); debug(":");
+        debug_hex(gs_uc_mac_address[3]); debug(":");
+        debug_hex(gs_uc_mac_address[4]); debug(":");
+        debug_hex(gs_uc_mac_address[5]); debug("\r\n");
 
-	printf("-- IP  %d.%d.%d.%d\r\n", gs_uc_ip_address[0], gs_uc_ip_address[1],
-			gs_uc_ip_address[2], gs_uc_ip_address[3]);
+        debug("IP: ");
+        debug(gs_uc_ip_address[0]); debug(".");
+        debug(gs_uc_ip_address[1]); debug(".");
+        debug(gs_uc_ip_address[2]); debug(".");
+        debug(gs_uc_ip_address[3]); debug("\r\n");
 
 	// Reset PHY
 	rstc_set_external_reset(RSTC, 13); // (2^(13+1))/32768
@@ -371,36 +343,35 @@ void ether_init() {
 
 	gs_emac_dev.p_hw = EMAC;
   
-        printf("Init EMAC driver structure\r\n");
+        debug("Init EMAC driver structure\r\n");
 	// Init EMAC driver structure
 	emac_dev_init(EMAC, &gs_emac_dev, &emac_option);
 
 	// Enable Interrupt
 	NVIC_EnableIRQ(EMAC_IRQn);
         
-        printf("Init MAC PHY driver\r\n");
+        debug("Init MAC PHY driver\r\n");
 
 	// Init MAC PHY driver
 	if (ethernet_phy_init(EMAC, BOARD_EMAC_PHY_ADDR, SystemCoreClock)
 					!= EMAC_OK) {
-		puts("PHY Initialize ERROR!\r\n");
+		debug("PHY Initialize ERROR!\r\n");
 		//return -1;
 	}
 
 	// Auto Negotiate, work in RMII mode
 	if (ethernet_phy_auto_negotiate(EMAC, BOARD_EMAC_PHY_ADDR) != EMAC_OK) {
 
-		puts("Auto Negotiate ERROR!\r\n");
+		debug("Auto Negotiate ERROR!\r\n");
 		//return -1;
 	}
         
-        Serial.println("Establish ethernet link");
+        debug("Establish ethernet link... ");
 	// Establish ethernet link
 	while (ethernet_phy_set_link(EMAC, BOARD_EMAC_PHY_ADDR, 1) != EMAC_OK) {
-          Serial.println(EMAC_OK);
-          puts("Set link ERROR!\r\n");
-		//return -1;
+          debug("ERROR\r\n");
 	}
+        debug("OK\r\n");
 }
 
 void ether_recv() {
